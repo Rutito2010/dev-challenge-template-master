8c133551432a665012f571b5874e8d15ac965556
Rutito2010 <kakaroto11@live.com.ar>
2022-05-03 14:26:26 -0300

    Reacomodo de apollo y estructura basica

--- kimchechallenge/src/App.js
+++ kimchechallenge/src/App.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
 
-const client = new ApolloClient({
-  uri: "https://countries.trevorblades.com/",
-});
-
-const App = () => (
-  <ApolloProvider client={client}>
+import { gql, useQuery } from "@apollo/client";
+const GET_COUNTRIES = gql`
+  query {
+    continents {
+      name
+      countries {
+        name
+      }
+    }
+  }
+`;
+const App = () => {
+  const { data, error, loading } = useQuery(GET_COUNTRIES);
+  const [filter, setFilter] = useState(true);
+  console.log(data);
+  // const Continent= render(){ return(<div>
+  //         <p>Country List by Continent</p>
+  //         {data.continents.map((e) => (
+  //           <ul key={e.name}>{e.name}</ul>
+  //         ))}</div>)
+  // };
+  return (
     <div>
-      <h2>
-        My first Apollo app{" "}
-        <span role="img" aria-label="Rocket">
-          🚀
-        </span>
-      </h2>
+      <h1>Country Search</h1>
+      <h4>Introduce your country</h4>
+      <input />
+      <h2>Group by :</h2> <button>Continent</button>
+      <button>Language</button>
+      {loading ? (
+        <p>Loading Countries</p>
+      ) : (
+        <>
+          <p>Country List by Continent</p>
+          {data.continents.map((e) => (
+            <ul key={e.name}>{e.name}</ul>
+          ))}
+        </>
+      )}
+      {/* {filter === true ? <div>wd</div> : <div>batatas</div>} */}
     </div>
-  </ApolloProvider>
-);
+  );
+};
 export default App;
---
+++ kimchechallenge/src/Queries/Queries.jsx
@@ -0,0 +1,18 @@
+// import { gql } from "apollo-boost";
+
+// const get_Countries = gql`
+//   {
+//     continents {
+//       name
+//     }
+//   }
+// `;
+// const get_Languages = gql`
+//   {
+//     languajes {
+//       name
+//     }
+//   }
+// `;
+
+// export default { get_Countries, get_Languages };
