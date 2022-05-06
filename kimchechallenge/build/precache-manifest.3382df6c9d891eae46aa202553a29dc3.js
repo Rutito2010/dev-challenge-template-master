cacd4949904be6e960ed049b7fe3417685d7fd43
Rutito2010 <kakaroto11@live.com.ar>
2022-05-06 10:59:36 -0300

    build

---
+++ kimchechallenge/build/precache-manifest.3382df6c9d891eae46aa202553a29dc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f3d66ff6c21923dab963689106fc18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21e1d4079023c1757706",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "ce8e75f138d5ccd3754b",
+    "url": "/static/js/2.84564c6c.chunk.js"
+  },
+  {
+    "revision": "008e35d0f4dc1feb51eb85b24e9797eb",
+    "url": "/static/js/2.84564c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21e1d4079023c1757706",
+    "url": "/static/js/main.1a2354b9.chunk.js"
+  },
+  {
+    "revision": "f79177c24e5f8a16816b",
+    "url": "/static/js/runtime-main.6e9a7329.js"
+  }
+]);
