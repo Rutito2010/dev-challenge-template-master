f553a0db4feffe8411ff5440d5430802be1754cc
Rutito2010 <kakaroto11@live.com.ar>
2022-05-06 13:38:50 -0300

    estilizado

--- kimchechallenge/src/Components/byLanguage.jsx
+++ kimchechallenge/src/Components/byLanguage.jsx
@@ -1,27 +1,34 @@
 import React from "react";
+import "./byContinent.css";
 
 export const ByLanguages = ({ datita }) => {
   // const { data, error, loading } = useQuery(GET_LANGUAGES);
   // console.log(datita);
 
   if (datita === null || datita === undefined) {
-    return <div> Waiting for keyswords...</div>;
+    return <div> Waiting for keywords...</div>;
   } else {
     return (
       <div>
-        <p>Country List by Language</p>
         {datita.map((lan) => (
           <ul key={lan.name}>
-            <h2>-{lan.name}</h2>
+            <h1>{lan.name}</h1>
             {lan.countries.map((c) => (
-              <div key={c.name}>
-                <span role="img" aria-label="arrow">
-                  {c.flag}
-                </span>
-                <h3>{c.name}</h3>
-                <h4>{c.capital}</h4>
-                <h4>{c.continent}</h4>
-                <h4>{c.currency}</h4>
+              <div key={c.name} className="Cards">
+                <div className="cardTilte">
+                  <h2>
+                    <span className="flag" role="img" aria-label="arrow">
+                      {c.flag}
+                    </span>
+                    {"   "}
+                    {c.name}
+                  </h2>
+                </div>
+                <div className="info">
+                  <h4>Capital: {c.capital}</h4>
+                  <h4>Continent: {c.continent}</h4>
+                  <h4>Currency: {c.currency}</h4>
+                </div>
               </div>
             ))}
           </ul>
